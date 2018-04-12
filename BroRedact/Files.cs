35bc387664fef4a0955cd29d37db3940c6589ff9
DenBroShow <denchik.ktm@gmail.com>
2018-04-12 19:28:59 +0300

    Version 0.1
    First commit

---
+++ BroRedact/Files.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+
+namespace BroRedact
+{
+    class Files
+    {
+        private static string file;
+        private static string filestring;
+        private static string newstring;
+        private static int symbolstotal;
+        public static void LoadFile(string path)
+        {
+            file = path;
+            filestring = File.ReadAllText(path, Encoding.GetEncoding(1251));
+            newstring = filestring;
+        }
+
+        public static string GetFileName()
+        {
+            return file;
+        }
+
+        public static string GetFileContent()
+        {
+            return filestring;
+        }
+
+        public static void NewString(string str)
+        {
+            newstring = str;
+            symbolstotal = newstring.Length;
+        }
+
+        public static bool AreEqual()
+        {
+            return filestring != newstring;
+        }
+
+        public static void Save()
+        {
+            File.WriteAllText(file, newstring, Encoding.GetEncoding(1251));
+        }
+
+        public static int GetSymbols()
+        {
+            return symbolstotal;
+        }
+    }
+}
---
+++ BroRedact/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BroRedact
+{
+    public partial class Form1 : Form
+    {
+        public string[] loadstate;
+        public Form1(string[] args)
+        {
+            loadstate = args;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Загрузка формы
+        {
+            if (loadstate.Length > 0)
+            {
+                if (File.Exists(loadstate[0]))
+                {
+                    Files.LoadFile(loadstate[0]);
+                    textBox1.Text = Files.GetFileName();
+                    textBox2.Text = Files.GetFileContent();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // "Новый"
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Текстовый файл|*.txt|Файл BroRedact|*.bred";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                File.Create(dlg.FileName).Close();
+                Files.LoadFile(dlg.FileName);
+                textBox1.Text = Files.GetFileName();
+                textBox2.Text = Files.GetFileContent();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // "Выбрать"
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Текстовые файлы|*.txt;*.bred|Все файлы|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(dlg.FileName))
+                {
+                    Files.LoadFile(dlg.FileName);
+                    textBox1.Text = Files.GetFileName();
+                    textBox2.Text = Files.GetFileContent();
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) // Текстовое поле
+        {
+            Files.NewString(textBox2.Text);
+            label2.Text = "Символов: " + Files.GetSymbols();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // "Сохранить"
+        {
+            if (!Files.AreEqual()) MessageBox.Show("Вы не внесли никаких изменений в текущий файл. Документ не был сохранён", "Ошибка", MessageBoxButtons.OK);
+            else
+            {
+                Files.Save();
+                MessageBox.Show("Файл сохранён", "Успех", MessageBoxButtons.OK);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) // Строка с файлом
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) // Кол-во символов
+        {
+            
+        }
+    }
+}
